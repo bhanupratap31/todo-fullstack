5bf8e840ebb963062011f60d23231cb752620713
Bhanu Pratap Shukla <bhanu@Bhanus-MacBook-Pro.local>
2024-10-21 23:41:55 +0530

    Frontend

---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,10 @@
+
+export function Todos(){
+    return(
+        <div>
+            <h1>Gym</h1>
+            <h1>Go to the gym</h1>
+            <button>Mark as completed</button>
+        </div>
+    )
+}
